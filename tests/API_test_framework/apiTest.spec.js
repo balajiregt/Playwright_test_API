81a7d26b968acd4cce2ff9956eac28e5614a29e2
BalajiK <71222979+balajiregt@users.noreply.github.com>
2023-11-13 19:56:18 +0530

    Branch a (#17)
    * added api boilerplate project
    
    * added request url logs
    
    * added readme file
    
    * updated jenkinsfile
    
    * updated jenkinfile, added sh scripts
    
    * updated jenkinfile urls
    
    * updated sh2
    
    * added broser deps
    
    * added shell load script
    
    * added schema validation
    
    * added schema tests and files
    
    ---------
    
    Co-authored-by: Balaji <balaji@Balajis-MacBook-Air.local>

--- tests/API_test_framework/apiTest.spec.js
+++ tests/API_test_framework/apiTest.spec.js
@@ -1,14 +1,16 @@
 const { test, expect } = require('@playwright/test');
+const Ajv = require('ajv');
+const userSchema = require('./userSchema.json')
 const { getRequest, postRequest, putRequest, deleteRequest } = require('./requestUtility');
 const { saveData, getData } = require('./dataStore');
 const fs = require('fs').promises;
 
 test.describe.serial('User API Tests', () => {
     test('POST User', async () => {
-       // Read the JSON file
+        // Read the JSON file
         const newUser = JSON.parse(await fs.readFile('tests/API_test_framework/userData.json', 'utf8'));
         const response = await postRequest('/users', newUser);
-        
+
         expect(response.status()).toBe(201);
         const responseJson = await response.json();
         saveData('userId', responseJson.id); // Save the user ID for later use
@@ -16,20 +18,32 @@ test.describe.serial('User API Tests', () => {
         console.log('New user id', responseJson.id)
     });
 
-    test('GET User', async () => {
+    test('GET User - Validate JSON Schema', async () => {
         const userId = getData('userId');
         const response = await getRequest(`/users/${userId}`);
-        
+
         expect(response.status()).toBe(200);
         const userDetails = await response.json();
         console.log('Retrieved user details', userDetails)
+
+        // Initialize AJV and validate the response
+        const ajv = new Ajv();
+        const validate = ajv.compile(userSchema);
+        const valid = validate(userDetails);
+
+        if (!valid) {
+            console.error('AJV Validation Errors:', ajv.errorsText(validate.errors));
+        }
+        console.log('Schema validation is true')
+
+        expect(valid).toBe(true);
     });
 
     test('PUT User', async () => {
         const userId = getData('userId');
         const updatedUser = JSON.parse(await fs.readFile('tests/API_test_framework/updateUser.json', 'utf8'));
         const response = await putRequest(`/users/${userId}`, updatedUser);
-        
+
         expect(response.status()).toBe(200);
         const userDetails = await response.json();
         console.log('Retrieved user details', userDetails)
@@ -38,7 +52,7 @@ test.describe.serial('User API Tests', () => {
     test('DELETE User', async () => {
         const userId = getData('userId');
         const response = await deleteRequest(`/users/${userId}`);
-       
+
         expect(response.status()).toBe(204);
         const responseBody = await response.text();
         expect(responseBody).toBe('');
