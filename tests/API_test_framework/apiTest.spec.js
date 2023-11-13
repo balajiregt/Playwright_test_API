b02a90f7b8c786ca864a572e25154349e541b8a8
Balaji <balaji@Balajis-MacBook-Air.local>
2023-11-13 19:52:49 +0530

    added schema tests and files

--- tests/API_test_framework/apiTest.spec.js
+++ tests/API_test_framework/apiTest.spec.js
@@ -7,10 +7,10 @@ const fs = require('fs').promises;
 
 test.describe.serial('User API Tests', () => {
     test('POST User', async () => {
-       // Read the JSON file
+        // Read the JSON file
         const newUser = JSON.parse(await fs.readFile('tests/API_test_framework/userData.json', 'utf8'));
         const response = await postRequest('/users', newUser);
-        
+
         expect(response.status()).toBe(201);
         const responseJson = await response.json();
         saveData('userId', responseJson.id); // Save the user ID for later use
@@ -18,23 +18,13 @@ test.describe.serial('User API Tests', () => {
         console.log('New user id', responseJson.id)
     });
 
-    test('GET User', async () => {
-        const userId = getData('userId');
-        const response = await getRequest(`/users/${userId}`);
-        
-        expect(response.status()).toBe(200);
-        const userDetails = await response.json();
-        console.log('Retrieved user details', userDetails)
-    });
-
     test('GET User - Validate JSON Schema', async () => {
         const userId = getData('userId');
         const response = await getRequest(`/users/${userId}`);
-        
-        expect(response.status()).toBe(200);
 
+        expect(response.status()).toBe(200);
         const userDetails = await response.json();
-        console.log('Retrieved user details', userDetails);
+        console.log('Retrieved user details', userDetails)
 
         // Initialize AJV and validate the response
         const ajv = new Ajv();
@@ -44,6 +34,7 @@ test.describe.serial('User API Tests', () => {
         if (!valid) {
             console.error('AJV Validation Errors:', ajv.errorsText(validate.errors));
         }
+        console.log('Schema validation is true')
 
         expect(valid).toBe(true);
     });
@@ -52,7 +43,7 @@ test.describe.serial('User API Tests', () => {
         const userId = getData('userId');
         const updatedUser = JSON.parse(await fs.readFile('tests/API_test_framework/updateUser.json', 'utf8'));
         const response = await putRequest(`/users/${userId}`, updatedUser);
-        
+
         expect(response.status()).toBe(200);
         const userDetails = await response.json();
         console.log('Retrieved user details', userDetails)
@@ -61,7 +52,7 @@ test.describe.serial('User API Tests', () => {
     test('DELETE User', async () => {
         const userId = getData('userId');
         const response = await deleteRequest(`/users/${userId}`);
-       
+
         expect(response.status()).toBe(204);
         const responseBody = await response.text();
         expect(responseBody).toBe('');
